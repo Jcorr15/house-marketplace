50fe198212d21ebf1e7e10cc76995721adf30a0a
Jovan <jovancorri1@gmail.com>
2022-08-08 22:37:21 -0400

    Fixed Lint Warnings

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,5 +1,4 @@
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
@@ -15,6 +14,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+initializeApp(firebaseConfig);
 export const db = getFirestore();
